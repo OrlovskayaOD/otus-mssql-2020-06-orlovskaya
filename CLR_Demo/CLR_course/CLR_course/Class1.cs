670d6bcd7262fa77fa085a52a686649feebfe629
OrlovskayaOD <67789568+OrlovskayaOD@users.noreply.github.com>
2020-10-18 21:13:06 +0300

    Add files via upload

---
+++ CLR_Demo/CLR_course/CLR_course/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
+using System.Data;
+using System.Data.SqlTypes;
+
+namespace CLR_course
+{
+    public class Class1
+    {
+        public static int SumFunction(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static void NameProcedure(SqlString name)
+        {
+            var pipe = SqlContext.Pipe;
+
+            var columns = new SqlMetaData[1];
+            columns[0] = new SqlMetaData("HelloColumn", SqlDbType.NVarChar, 100);
+
+            var row = new SqlDataRecord(columns);
+            pipe.SendResultsStart(row);
+            for (int i = 0; i < 5; i++)
+            {
+                row.SetSqlString(0, string.Format("{0} Hello, {1}", i, name));
+                pipe.SendResultsRow(row);
+            }
+
+            pipe.SendResultsEnd();
+        }
+    }
+}
---
+++ CLR_Demo/CLR_course/CLR_course/Подключение dll.sql
@@ -0,0 +1,73 @@
+use WideWorldImporters;
+
+---- Чистим от предыдущих экспериментов
+--DROP FUNCTION IF EXISTS dbo.fn_SumFunc
+--GO
+--DROP PROCEDURE IF EXISTS dbo.usp_SayHello
+--GO
+--DROP ASSEMBLY IF EXISTS SimpleDemoAssembly
+--GO
+
+-- Включаем CLR
+exec sp_configure 'show advanced options', 1;
+go
+reconfigure;
+go
+
+exec sp_configure 'clr enabled', 1;
+exec sp_configure 'clr strict security', 0 
+go
+
+reconfigure;
+go
+
+-- Для EXTERNAL_ACCESS или UNSAFE
+alter database WideWorldImporters set trustworthy on; 
+
+-- Подключаем dll 
+create assembly DemoAssembly
+from 'C:\CLR_Demo\CLR_course\CLR_course\bin\Debug\CLR_course.dll'
+with permission_set = safe;  
+
+-- DROP ASSEMBLY SimpleDemoAssembly
+
+-- Подключить функцию из dll
+create function dbo.fn_SumFunc(@Num1 int, @Num2 int)  
+returns int
+as external name [DemoAssembly].[CLR_course.Class1].SumFunction;
+go 
+
+-- Без namespace будет так:
+-- [SimpleDemoAssembly].[DemoClass].SayHelloFunction
+
+-- Используем функцию
+select 
+   dbo.fn_SumFunc(2,3) as [2+3], 
+   dbo.fn_SumFunc(4,5) as [4+5]
+
+-- Подключить процедуру из dll
+create procedure dbo.usp_DemoName 
+(  
+    @Name nvarchar(50)
+)  
+AS EXTERNAL NAME  [DemoAssembly].[CLR_course.Class1].NameProcedure;  
+GO 
+
+-- Используем ХП
+exec dbo.usp_DemoName @Name = 'Oksana Orlovskaya';
+
+-- --------------------------
+
+-- Как посмотреть зарегистрированные сборки 
+
+-- SSMS
+-- <DB> -> Programmability -> Assemblies 
+
+-- Посмотреть подключенные сборки (SSMS: <DB> -> Programmability -> Assemblies)
+SELECT * FROM sys.assemblies
+
+-- Проверяем, что создали ХП и функцию
+SELECT * FROM sys.assembly_modules
+
+-- Посмотреть "код" сборки
+-- SSMS: <DB> -> Programmability -> Assemblies -> Script Assembly as ...
